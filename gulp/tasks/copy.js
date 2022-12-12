1179e5d3a3332841edfadbb02d6003f3eb7d656f
sirMaxF <sirmaxfrei@mail.ru>
2022-12-12 14:31:47 +0300

    3

--- gulp/tasks/copy.js
+++
@@ -1,6 +0,0 @@
-// это файл для задачи копирования файлов
-
-export const copy = () => { // export ставим чтобы эту функцию можно в других местах использовать
-  return app.gulp.src(app.path.src.files) // получаем доступ к данным из файла path.js, там в export const path есть объект src
-    .pipe(app.gulp.dest(app.path.build.files)) // определяем куда файлы перенести
-};
--- gulp/tasks/server.js
+++
@@ -1,9 +0,0 @@
-export const server = (done) => {
-    app.plugins.browsersync.init({ // init запускает плагин browsersync
-        server: {
-            baseDir: `${app.path.build.html}`, // запускаем папку проекта baseDir
-            notify: false,
-            port: 3000,
-        }
-    })
-}
